2ce029e3dc519b163fe6245601ed0ce6e28755ab
matthieu bolia <mathiasbolia82@gmail.com>
2026-09-09 10:21:33 +0100

    Change default API_BASE_URL to an empty string

--- lib/core/network/configuration_api.dart
+++ lib/core/network/configuration_api.dart
@@ -6,7 +6,7 @@ abstract final class ConfigurationApi {
 
   static const urlBase = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://camping-overwrite-upstate.ngrok-free.dev',
+    defaultValue: ' ',
   );
 
   static const dureeExpiration = Duration(seconds: 30);
